350a914eaba549fb95b91788a8ec53a91d4196e8
abbashirkani <31324728+abbashirkani@users.noreply.github.com>
2017-09-01 19:17:09 +0530

    Create assignment2pb1.cpp

---
+++ assignment2pb1.cpp
@@ -0,0 +1,16 @@
+# abbasassignment2
+// question 1
+
+include<iostream>
+using namespace std;
+int main()
+{
+ float a,b,c;
+ cout << "enter length in cm";
+ cin >> a;
+ b=a/100;
+ c=a/100000;
+ cout<< "length in meter  ="<<b;
+ cout << "length in kilometer ="<<c;
+ return 0;
+}
