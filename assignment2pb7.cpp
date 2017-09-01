b0c31d2521e2f06e9641a157bdcd240fa4b92174
abbashirkani <31324728+abbashirkani@users.noreply.github.com>
2017-09-01 19:25:43 +0530

    Create assignment2pb7.cpp

---
+++ assignment2pb7.cpp
@@ -0,0 +1,13 @@
+# abbasassignment2
+/ question 7
+#include<iostream>
+using namespace std;
+int main()
+{
+ float a,b,c;
+ cout<<"Enter tha two angles of the triangle :";
+ cin>>a>>b;
+ c=180-(a+b);
+ cout<<"thrid angle is:"<<c;
+ return 0;
+}
