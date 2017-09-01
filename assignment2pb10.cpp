f909fc1a63e4b9bdbc819604ab7c9e29481a443c
abbashirkani <31324728+abbashirkani@users.noreply.github.com>
2017-09-01 19:31:13 +0530

    Create assignment2pb10.cpp

---
+++ assignment2pb10.cpp
@@ -0,0 +1,17 @@
+# abbasassignment2
+// question 10
+#include<iostream>
+using namespace std;
+int main()
+{
+ float a,b,c,d,f,g,h,t,p;
+ cout<<"Enter the marks of five subject of a student :";
+ cin>>b>>c>>d>>f>>g>>h;
+ t=b+c+d+f+g+h;
+ a=t/5;
+ p=(t*100)/500;
+ cout<<"total is : "<<t;
+ cout<<endl<<"average : "<<a;
+ cout<<endl<<"Precentage : "<<p;
+ return 0;
+}
